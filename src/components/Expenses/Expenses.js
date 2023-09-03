b06408c2be655b480b7c4b649752c5e8f895c279
Derek Kweon <kdh4646@hanmail.net>
2023-09-02 18:02:34 -0700

    2023-09-02 implement filter()

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,7 +9,7 @@ const Expenses = (props) => {
 
   /* 
     Derived state
-    
+
     let filterInfoText = "2019, 2021, 2022 & 2023";
 
     if (filteredYear === "2019") {
@@ -27,6 +27,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   //Whenever combine components you are using composition => ex) Card
   return (
     <div>
@@ -35,26 +39,15 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {/* Dynamic rendering using map() */}
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
