9ca43c9aad704313a791e9dcb5c2de1018a299df
Derek Kweon <kdh4646@hanmail.net>
2023-09-02 18:12:19 -0700

    2023-09-02 add conditional statemnt

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -31,6 +31,18 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   //Whenever combine components you are using composition => ex) Card
   return (
     <div>
@@ -39,15 +51,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {/* Dynamic rendering using map() */}
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
